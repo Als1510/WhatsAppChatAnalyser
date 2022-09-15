ace596bb36f05913bef2e4c8cbaaa9ce71361f13
Als1510 <lekhrajsharmaas@gmail.com>
2022-09-15 17:21:40 +0530

    feat: Chat From, Most Talkative, Fetch Stats, Weekly & Monthly Timeline

--- app.py
+++ app.py
@@ -26,8 +26,36 @@ if uploaded_file is not None:
   selected_user = st.sidebar.selectbox("Show analysis w.r.t.", user_list)
 
   if st.sidebar.button("Show Analysis"):
+    # Top Statistics
+    st.header('Top Statistics')
+    num_messages, words, num_media_messages = helper.fetch_stats(selected_user, df)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+      st.markdown('<h2 style="color:Blue; text-align:center; padding-bottom: 0;">'+str(num_messages)+'</h2>',unsafe_allow_html=True)
+      st.markdown('<h4 style="color:Blue; text-align:center;">Message</h4>',unsafe_allow_html=True)
+    with col2:
+      st.markdown('<h2 style="color:Blue; text-align:center; padding-bottom: 0">'+str(len(user_list)-1)+'</h2>',unsafe_allow_html=True)
+      st.markdown('<h4 style="color:Blue; text-align:center;">Users</h4>',unsafe_allow_html=True)
+    with col3:
+      st.markdown('<h2 style="color:Blue; text-align:center; padding-bottom: 0">'+str(words)+'</h2>',unsafe_allow_html=True)
+      st.markdown('<h4 style="color:Blue; text-align:center;">Words</h4>',unsafe_allow_html=True)
+    with col4:
+      st.markdown('<h2 style="color:Blue; text-align:center; padding-bottom: 0">'+str(num_media_messages)+'</h2>',unsafe_allow_html=True)
+      st.markdown('<h4 style="color:Blue; text-align:center;">Media</h4>',unsafe_allow_html=True)
 
-    st.dataframe(df)
+    # Chatting From
+    year, avg_msg, username = helper.chat_from(selected_user, df)
+    st.markdown('<h3 style="text-align:center; margin-top: 0.5rem">'+ username + ' have been chatting since ' + '<span style="color: #7dc580">' + str(year) + '</span>' + ' with an average of ' + '<span style="color: #7dc580">' + str(avg_msg) + '</span> message per day!</h3>',unsafe_allow_html=True)
+
+    # Most talkative
+    img_src = "data:image/png;base64,{}".format(helper.img_to_bytes("msg_icon.png"))
+    img = '<img src="' + img_src + '" style="height: 30px" margin: 0;>'
+    st.markdown('<div style="text-align:center; margin: 1.5rem 0 0.3rem 0; display: flex; flex-direction: row; align-items:center; justify-content:center;">' + img + '<h2 style=" padding: 0;"> Most Talkative </h2>' + img + '</div>', unsafe_allow_html=True)
+    
+    username, avg_msg = helper.most_talkative(df)
+    st.markdown('<h4 style="text-align:center; padding: 0.4rem 0;">' + username + '</h4>', unsafe_allow_html=True)
+    st.markdown('<h5 style="text-align:center;">' + str(avg_msg) + '% of the total messages</h5>', unsafe_allow_html=True)
+
     # Messages sent by
     st.header('Message Sent')
 
@@ -51,10 +79,30 @@ if uploaded_file is not None:
       pan_zoom='zoom'
     )
 
+    # By Week
+    st.subheader('By Week')
+    new_df = helper.weekly_timeline(selected_user, df)
+    plost.line_chart(
+      new_df,
+      x = 'Days',
+      y = 'Message',
+      pan_zoom='zoom'
+    )
+
+    # By Month
+    st.subheader('By Month')
+    new_df = helper.monthly_timeline(selected_user, df)
+    plost.line_chart(
+      new_df,
+      x = 'Month',
+      y = 'Message',
+      pan_zoom='zoom',
+    )
+    
+
     # Most common words
     st.header('Most common words')
     new_df = helper.most_common_words(selected_user, df)
-    st.dataframe(new_df)
     plost.bar_chart(
       new_df,
       bar = 'Message',
--- helper.py
+++ helper.py
@@ -1,10 +1,46 @@
 from collections import Counter
 import streamlit as st
 import pandas as pd
+import base64
+from pathlib import Path
 
 def showError(message):
   st.error(message)
   st.stop()
+
+def img_to_bytes(img_path):
+  img_bytes = Path(img_path).read_bytes()
+  encoded = base64.b64encode(img_bytes).decode()
+  return encoded
+
+def chat_from(selected_user, df):
+  if selected_user != 'Overall':
+    df = df[df['User'] == selected_user]
+  else:
+    selected_user = 'You'
+  unique_years = df['Year'].unique()
+  start_year = unique_years[0]
+  msg_count = df.groupby(['Date']).count()['Message']
+  avg_msg = round(msg_count.mean(),2)
+  return start_year, avg_msg, selected_user
+
+def most_talkative(df):
+  df = df[df['User'] != 'group_notification']
+  user = df['User'].value_counts()
+  username = user.index[0]
+  avg_msg = round(len(df)/user[username], 2)
+  return username, avg_msg
+  
+def fetch_stats(selected_user, df):
+  if selected_user != 'Overall':
+    df = df[df['User'] == selected_user]
+  num_messages = df.shape[0]
+  words = []
+  for message in df['Message']:
+    words.extend(message.split())
+
+  num_media_messages = df[df['Message'] == '<Media omitted>\n'].shape[0]
+  return num_messages, len(words), num_media_messages
 
 def hourly_timeline(selected_user, df, format):
   if selected_user != 'Overall':
@@ -27,6 +63,29 @@ def daily_timeline(selected_user, df):
   new_df['Message'] = [1] * new_df.shape[0]
   new_df = new_df.groupby('Date').sum().reset_index()
   return new_df
+
+def weekly_timeline(selected_user, df):
+  if selected_user != 'Overall':
+    df = df[df['User'] == selected_user]
+  new_df = df[df['User'] != 'group_notification']
+  
+  new_df['Message'] = [1] * new_df.shape[0]
+  days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  new_df = new_df.groupby('DayName').sum().reindex(days)
+  new_df['Days'] = ['1 - Monday', '2 - Tuesday', '3 - Wednesday', '4 - Thursday', '5 - Friday', '6 - Saturday', '8 - Sunday']
+  return new_df
+
+def monthly_timeline(selected_user, df):
+    if selected_user != 'Overall':
+      df = df[df['User'] == selected_user]
+    new_df = df[df['User'] != 'group_notification']
+    new_df  = df.groupby(['Year','Month','MonthNum']).count()
+    new_df.reset_index(inplace=True)
+    time = []
+    for i in range(new_df.shape[0]):
+        time.append(str(new_df['MonthNum'][i]) + " - " + str(new_df['Year'][i]) + " - " + new_df['Month'][i])
+    new_df['Month'] = time
+    return new_df
 
 def most_common_words(selected_user, df):
   if selected_user != 'Overall':
